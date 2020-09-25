83d271f0b99df082a1ab513f1b7da2d27f7e0030
Xavier Defontaine <xdefontaine@gmail.com>
2020-09-25 17:40:43 +0100

    end of chapter 14

--- lib/database_connection_setup.rb
+++ lib/database_connection_setup.rb
@@ -3,5 +3,5 @@ require_relative 'database_connection'
 if ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup('bookmark_manager_test') 
 else
-  DatabaseConnection.setup('bookmark-manager')#change back to underscore
+  DatabaseConnection.setup('bookmark_manager')#change back to underscore
 end
